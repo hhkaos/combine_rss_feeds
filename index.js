f8bd1742d0e77398ddb5c806ffbee07f280b4b70
Raul Jimenez Ortega <rjimenez@esri.com>
2025-11-26 15:00:13 +0100

    Add new RSS feed for Esri jsapi resources

--- index.js
+++ index.js
@@ -433,6 +433,7 @@ function getDateString() {
     'https://josiahparry.com/index.xml',
     'https://highearthorbit.com/feed/',
     'https://github.com/esri/developer-support/commits/master.atom',
+    'https://github.com/Esri/jsapi-resources/commits.atom',
     'https://www.esri.com/arcgis-blog/products/developers/feed',
     'https://community.esri.com/ccqpr47374/rss/board?board.id=python-blog',
     'https://www.esri.com/arcgis-blog/feed/?post_type=blog&product=developers',
