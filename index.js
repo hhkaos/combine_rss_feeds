cf4edf7f9b67329fb85598385d3c2d82368f18c6
Raul Jimenez Ortega <rjimenez@esri.com>
2026-03-03 15:08:46 +0100

    Add new RSS feed for Adventures in Mapping

--- index.js
+++ index.js
@@ -432,6 +432,7 @@ function getDateString() {
     'https://odoe.net/rss.xml',
     'https://josiahparry.com/index.xml',
     'https://highearthorbit.com/feed/',
+    'https://adventuresinmapping.com/feed/',
     'https://github.com/esri/developer-support/commits/master.atom',
     'https://github.com/Esri/jsapi-resources/commits.atom',
     'https://www.esri.com/arcgis-blog/products/developers/feed',
